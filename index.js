00f83bcd7f302025f25921d66b9d8d80e622f3d5
TBHGodPro <tarun.bhaskar@outlook.com>
2023-02-17 13:56:21 -0600

    Use unofficial ChatGPT-API

--- index.js
+++ index.js
@@ -1,50 +1,54 @@
-import express from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import env from 'dotenv'
-import {Configuration, OpenAIApi} from 'openai'
+import bodyParser from 'body-parser';
+import { ChatGPTAPI } from 'chatgpt';
+import cors from 'cors';
+import env from 'dotenv';
+import express from 'express';
+import fetch from 'node-fetch';
 
-const app = express()
+const app = express();
 
-env.config()
+env.config();
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-
-// Configure open api
-const configuration = new Configuration({
-    organization: "API_ORGANIZATION_THING",
-    apiKey: process.env.API_KEY // VISIT .env AND MAKE CHANGES
-})
-const openai = new OpenAIApi(configuration)
-
-
-// listeninng
-app.listen("3080", ()=>console.log("listening on port 3080"))
-
+// Initialize ChatGPT
+const chatgpt = new ChatGPTAPI({
+	apiKey: process.env.API_KEY,
+	fetch
+});
 
 // dummy route to test
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
-
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
 
 //post route for making requests
-app.post('/', async (req, res)=>{
-    const {message} = req.body
+app.post('/', async (req, res) => {
+	const { message } = req.body;
+	try {
+		let response;
+		if (req.body.conversationId) {
+			response = await chatgpt
+				.sendMessage(message, {
+					conversationId: req.body.conversationId,
+					parentMessageId: req.body.lastMessageId,
+					timeoutMs: 30 * 1000 // 30 Second timeout
+				})
+				.catch(() => res.sendStatus(400));
+		} else {
+			response = await chatgpt
+				.sendMessage(message, {
+					timeoutMs: 30 * 1000 // 30 Second timeout
+				})
+				.catch(() => res.sendStatus(400));
+		}
+		if ('text' in response) res.json(response);
+	} catch (e) {
+		console.error(e);
+		res.sendStatus(400);
+	}
+});
 
-    try{
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${message}`,
-            max_tokens: 100,
-            temperature: .5
-        })
-        res.json({message: response.data.choices[0].text})
-
-    }catch(e){
-        console.log(e)
-        res.send(e).status(400)
-    }
-})
+// listeninng
+app.listen('3080', () => console.log('listening on port 3080'));
